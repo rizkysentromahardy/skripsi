b98dc3c6fc27abc2fc8b9624922fc9f75064354f
rizkysentromahardy <rizkysentro3@gmail.com>
2020-05-03 11:16:46 +0700

    firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import AppRouter from "./navigation";
+import firebase from "@react-native-firebase/app/";
+import database from '@react-native-firebase/database';
 class App extends Component {
     constructor(props) {
         super(props);
@@ -12,6 +14,18 @@ class App extends Component {
         // setTimeout(() => {
         //     this.setState({ isSignin: true })
         // }, 5000);
+        database()
+            .ref('/users/123')
+            .set({
+                name: 'Ada Lovelace',
+                age: 31,
+            })
+            .then(() => console.log('Data set.'));
+        database()
+            .ref('/users/123')
+            .on('value', snapshot => {
+                console.log('User data: ', snapshot.val());
+            });
     }
 
     render() {
